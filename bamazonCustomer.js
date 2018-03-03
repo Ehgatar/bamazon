60ea6c63c5d0d42a71961b6b60516d9ad1b0e11b
Edgar Castellanos <edgarcast7@gmail.com>
2018-03-03 10:02:58 -0600

    submitted wrong code prior to this. Correct code is this

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,7 @@
 var mysql = require("mysql");
+var inquirer = require('inquirer');
+const cTable = require('console.table');
+var Table = require('easy-table')
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -28,31 +31,76 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    runSearch();
+    console.log("connected as id " + connection.threadId);
+  runSearch();
   });
+  
   
   function runSearch() {
     inquirer
       .prompt({
         name: "action",
         type: "list",
-        message: "What would you like to order?",
+        message: "Please Choose [Display Products] if you do not know what you want to pruchase",
         choices: [
-          "type in the product you want",
+          "Display Products",
+          "Lets Begin our purchase",
+          
         ]
       })
       .then(function(answer) {
         switch (answer.action) {
-          case "Find the product":
-            artistSearch();
+          case "Display Products":
+            displayProducts();
+            break;
+  
+          case "Lets Begin our purchase":
+            idSearch();
+            break;
+  
+  
+            default:
+            console.log("Didnt hit");
+            console.log(answer);
             break;
         }
-      });
-    
   
-// var checkAndBuy2 = function() {
-//     connection.query('SELECT * FROM products', function(err, res) {
-//         //CREATES A NEW TABLE IN THE COOL CLI VIEW 
-//         var table = new Table({
-//             head: ['if', 'product_name', 'department', 'price', 'stock_quantity']
-//         }); 
+        function displayProducts() {
+          
+          console.table("Selecting all products...\n");
+  
+          connection.query("SELECT * FROM products", function(err, res) {
+            if (err) throw err;
+            // Log all results of the SELECT statement
+            console.table(res);
+            idSearch(res);
+            // connection.end();
+    
+          })
+          connection.end();
+        }
+  
+        function idSearch(response) {
+          
+          inquirer
+          .prompt({
+            name: "id",
+            type: "input",
+            message: "choose an ID"
+          })
+          .then(function(answer) {
+            var query = "SELECT product_name, department, price, stock_quantity FROM products WHERE ?";
+            connection.query(query, { id: answer.id }, function(err, res) {
+              for (var i = 0; i < res.length; i++) {
+                console.table("Product: " + res[i].product_name + " || Department: " + res[i].department_name + " || price: " + res[i].price  + " || stock quantity: " + res[i].stock_quantity);
+              }
+              // runSearch();
+            });
+          });
+  
+        }
+  
+      });
+  }
+  
+  
