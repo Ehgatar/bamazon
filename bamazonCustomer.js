f5def485995891cafb85373e0700fe0f5daddbc5
Edgar Castellanos <edgarcast7@gmail.com>
2018-03-02 22:47:57 -0600

    customer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,3 +11,22 @@ var connection = mysql.createConnection({
   password: "7500Frontage",
   database: "bamazon_db"
 });
+
+
+// // validateInput makes sure that the user is supplying only positive integers for their inputs
+// function validateInput(value) {
+// 	var integer = Number.isInteger(parseFloat(value));
+// 	var sign = Math.sign(value);
+
+// 	if (integer && (sign === 1)) {
+// 		return true;
+// 	} else {
+// 		return 'Please enter a whole non-zero number.';
+// 	}
+// }
+var checkAndBuy2 = function() {
+    connection.query('SELECT * FROM products', function(err, res) {
+        //CREATES A NEW TABLE IN THE COOL CLI VIEW 
+        var table = new Table({
+            head: ['if', 'product_name', 'department', 'price', 'stock_quantity']
+        });
